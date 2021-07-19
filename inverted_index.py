8143ef915372d21d9eaac5ffa2a030f3c9d48144
Luca <66808669+LucaPalmer@users.noreply.github.com>
2021-07-19 15:44:47 +0100

    Add files via upload

--- inverted_index.py
+++ inverted_index.py
@@ -9,24 +9,55 @@ data = pd.read_json(r'C:\Users\lucap\PycharmProjects\pythonProject\crawlertest\d
 #data.reset_index(inplace = True)
 #data = data.rename(columns = {'index': 'ID'})
 """
+# Convert columns to string format (Spacy only takes string as input).
+def string_conv(x):
+    x = str(x)
+    x = x.replace("\n", " ")
+    x = x.lower()
+    x = x.strip()
+    return (x)
 
-titles = data['Title'].to_string()  # Convert column to string format (Spacy only takes string as input)
+titles = string_conv(data['Title'])
+authors = string_conv(data['Non-EEC Authors'])
+eec_authors = string_conv(data['EEC Authors'])
+
+nlp = spacy.load("en_core_web_sm")  # Load trained pipeline for Spacy.
+
+# Create documents from using trained pipeline - Tokenisation already occurs here.
+titles_docs = nlp(titles)
+authors_docs = nlp(authors)
+eec_authors_docs = nlp(eec_authors)
 
 """
-authors = data['Non-EEC Authors'].astype(str)
-eec_authors = data['EEC Authors'].astype(str)
-years = data['Year'].astype(str)
-"""
-
-nlp = spacy.load("en_core_web_sm")  # Load trained pipeline for Spacy
-titles_doc = nlp(titles)  # Create documents from using trained pipeline
-
-
-# Tokenizer function - documents must be input into function
+# Tokenizer function for tokenising elements into terms - documents must be input into function.
 def tokeniser(doc):
     result = []
     for token in doc:
         result.append(token.text)
-    return(result)
+    return (result)
 
-titles_docs = tokeniser(titles_doc)
+
+titles_docs = tokeniser(titles_docs)
+author_docs = tokeniser(authors_docs)
+eec_authors_docs = tokeniser(eec_authors_docs)
+years_docs = tokeniser(years_docs)
+"""
+
+
+# Stop Word function for removing unnecessary terms - Requires (tokenized) documents.
+def remove_stop_words(doc):
+    tokens = []
+    for token in doc:
+        if not token.is_stop:
+            tokens.append(token.text)
+    return (tokens)
+
+# Remove Stop Words
+titles_docs = remove_stop_words(titles_docs)
+authors_docs = remove_stop_words(authors_docs)
+eec_authors_docs = remove_stop_words(eec_authors_docs)
+
+# Convert back to String Format
+titles_docs = string_conv(titles_docs)
+authors_docs = string_conv(authors_docs)
+eec_authors_docs = string_conv(eec_authors_docs)
