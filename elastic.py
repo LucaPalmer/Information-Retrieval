b2235e12817674832cced115c12eaa00b9f8924a
Luca <66808669+LucaPalmer@users.noreply.github.com>
2021-07-30 18:05:54 +0100

    Update elastic.py

--- elastic.py
+++ elastic.py
@@ -1,105 +1,86 @@
-# MongoDB password: UmIG9O9HrCUxXETt
-
 import json
-import pandas as pd
+from elasticsearch import Elasticsearch
+
+
+# Command Line Interface
+import fire
+
 
 with open('C:/Users/lucap/PycharmProjects/pythonProject/crawlertest/data.json') as f:
     data = json.load(f)
-
-import elasticsearch
-from elasticsearch import Elasticsearch
-
 es = Elasticsearch()
+
 
 # Add documents to index
 
 es.indices.refresh('test-index')
 es.cat.count('test-index', params={"format": "json"})
 
-
 def add_doc(doc):
     for i in range(len(doc)):
         es.index(index="test-index", id=i, body=doc[i])
+        es.indices.refresh('test-index')
 
-
-# add_doc(data)
-es.indices.refresh('test-index')
-
-"""
-# Get documents
-res = es.get(index="test-index", id=6)
-print(res['_source'])
-"""
-
-def title_query():
+def title():
     print("Enter a title: ")
     query = input()
-    title_query_body = {
+    title_body = {
       "query": {
           "match": {
               "Title": query
           }
       }
     }
-    title_res = es.search(index = "test-index", body = title_query_body)
+    title_res = es.search(index = "test-index", body = title_body)
     print("query hits:", title_res["hits"]["hits"])
 
-def author_query():
+def author():
     print("Enter an author name: ")
     query = input()
-    author_query_body = {
-      "query": {
-          "match": {
-              "Non-EEC Authors": query
-          }
-      }
+    author_body = {
+        "query": {
+            "match": {
+                "Non-EEC Authors": query,
+            }
+        }
     }
-    title_res = es.search(index = "test-index", body = author_query_body)
-    print("query hits:", title_res["hits"]["hits"])
-
-def EEC_query():
-    print("Enter an author name: ")
-    query = input()
-    EEC_query_body = {
-      "query": {
-          "match": {
-              "EEC Authors": query
-          }
-      }
+    EEC_author_body = {
+        "query": {
+            "match": {
+                "EEC Authors": query
+            }
+        }
     }
-    title_res = es.search(index = "test-index", body = EEC_query_body)
-    print("query hits:", title_res["hits"]["hits"])
+    author_res = es.search(index="test-index", body=author_body)
+    EEC_author_res = es.search(index="test-index", body=EEC_author_body)
+    if author_res != "[]":
+        print("Non-EEC Authors: ", author_res["hits"]["hits"], "\n")
+    else:
+        print ("No results found!")
+    if EEC_author_res != "[]":
+        print("EEC Authors: ", EEC_author_res["hits"]["hits"])
+    else:
+        print ("No results found!")
 
-def year_query():
+
+
+
+def year():
     print("Enter a year: ")
     query = input()
-    year_query_body = {
+    year_body = {
       "query": {
           "match": {
               "Year": query
           }
       }
     }
-    title_res = es.search(index = "test-index", body = year_query_body)
-    print("query hits:", title_res["hits"]["hits"])
+    author_res = es.search(index = "test-index", body = year_body)
+    print("query hits:", author_res["hits"]["hits"])
 
 
-def search():
-    print("Please select a query type")
-    print("Type 'title', 'authors', 'eec', 'year'")
-    query_select = input()
-    if query_select == 'title':
-        title_query()
-    if query_select == 'authors':
-        author_query()
-    if query_select == 'eec':
-        EEC_query()
-    if query_select == 'year':
-        year_query()
 
-search()
-"""
-#SEE ALL DOCUMENTS IN INDEX:
-for doc in title_res['hits']['hits']:
-    print (doc['_id'], doc['_source'])
-"""
+if __name__ == '__main__':
+    fire.Fire({'title': title,
+             'author': author,
+             'year': year})
